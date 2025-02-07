33574029f79c7ca36e17ad4df2c8a1233b297bad
alinazarif <alinazarif06@gmail.com>
2025-02-07 22:59:50 +0500

    Add files via upload

---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "../app/Styles/footer.css";
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+        <p>Alinaz Arif...</p>
+    </div>
+  );
+};
+
+export default Footer;
